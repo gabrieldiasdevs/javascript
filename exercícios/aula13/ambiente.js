9ab2ace22f5f7da48f8e869413c42f35da81bd29
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-02-20 22:57:25 -0300

    do

--- exercícios/aula13/ambiente.js
+++ exercícios/aula13/ambiente.js
@@ -7,8 +7,16 @@ console.log('Tudo bem?')
 console.log('Tudo bem?')
 */
 
+/*
 var c = 1
 while (c <= 400) {
     console.log(`Passo ${c}`)
     c++
 }
+*/
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 100)
