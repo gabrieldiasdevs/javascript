64e65678da7fd9bf7bff1af563c941a9d8261b9f
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-02-20 21:05:42 -0300

    Condição Múltipla

--- exercícios/aula012/ex013.js
+++ exercícios/aula012/ex013.js
@@ -11,4 +11,26 @@ var diaSem = agora.getDay()
     6 = Sabádo
 */
 
-console.log(diaSem)
+switch(diaSem) {
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('Terça')
+        break
+    case 3:
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('Quinta')
+        break
+    case 5:
+        console.log('Sexta')
+        break
+    case 6:
+        console.log('Sábado')
+        break
+}
