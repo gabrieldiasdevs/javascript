4eb6470fffd913ec030151d4502d7a103b12f2ed
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-02-21 19:40:55 -0300

    indexof

--- exercícios/aula15/vetorNaTela.js
+++ exercícios/aula15/vetorNaTela.js
@@ -1,5 +1,7 @@
 let valores = [8, 1, 7, 4, 2, 9]
 
+valores.sort()
+
 // console.log(valores)
 
 /*
@@ -9,8 +11,12 @@ console.log(valores[2])
 console.log(valores[3])
 console.log(valores[4])
 console.log(valores[5])
-*/
 
 for (let pos = 0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+*/
+
+for (let pos in valores) {
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
