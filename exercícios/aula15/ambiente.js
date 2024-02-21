b2b51d3ef7b3140bcc50f2ed8a6dd0d502e106a8
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-02-21 12:43:29 -0300

    array

--- exercícios/aula15/ambiente.js
+++ exercícios/aula15/ambiente.js
@@ -1,3 +1,7 @@
 let num = [5, 8, 2, 9, 3]
 
-console.log(`Nosso vetor é o ${num}`)
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
