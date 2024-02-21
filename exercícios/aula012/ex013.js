12cb34eeb54074de4935010a88467aa2ed1ecb9d
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-02-20 21:01:59 -0300

    getday

---
+++ exercícios/aula012/ex013.js
@@ -0,0 +1,14 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+
+/* 
+    0 = Domingo
+    1 = Segunda
+    2 = Terça
+    3 = Quarta
+    4 = Quinta
+    5 = Sexta
+    6 = Sabádo
+*/
+
+console.log(diaSem)
