84cb25abf2f07a7d3e0f69a85d014c7a633771d1
Gabriel Dias <158090647+gabrieldiasdevs@users.noreply.github.com>
2024-03-18 18:30:37 -0300

    desafio javascript

---
+++ desafios/desafio03/modelo/script.js
@@ -0,0 +1,9 @@
+function contador() {
+    var inicio = document.getElementById('ini')
+    var fim = document.getElementById('fim')
+    var passo = document.getElementById('pas')
+
+    if (inicio == ) {
+        window.alert('[ERRO] Digite um número e tente novamente!')
+    }
+}
